d574ab6b88c68617bfb47b066cab58abfde891f4
msz13 <mat.szczecinski@gmail.com>
2021-05-30 23:16:33 +0200

    added organizer registration test step implementation

---
+++ src/Zapisywarka.API/Modules/Identity/Zapisywarka.Api.Modules.Identity.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Zapisywarka.Api.Modules.Identity.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
