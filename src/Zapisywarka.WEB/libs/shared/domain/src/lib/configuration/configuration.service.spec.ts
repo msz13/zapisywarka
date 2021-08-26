a7bdf04e71e385b3b8ade34f5759438c57defdd4
msz13 <mat.szczecinski@gmail.com>
2021-08-26 21:13:45 +0200

    feat(web-shared-domain) implemented configuration service
    service should load configuration from file 'assets/config.json
    
    task: #31

---
+++ src/Zapisywarka.WEB/libs/shared/domain/src/lib/configuration/configuration.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ConfigurationService } from './configuration.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http';
+
+describe('ConfigurationService', () => {
+  let service: ConfigurationService;
+  let httpTestingController: HttpTestingController
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(ConfigurationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get config',  () => {
+
+    
+    const config = {
+      auth:  
+      {
+        issuer: '/',
+        clientId: 'spa',                
+      },
+      signUpURL: '/signUp'
+    }
+   
+     service.loadConfig().then(()=>{
+      expect(service.getConfig()).toStrictEqual(config);
+    })
+
+    
+
+    const req = httpTestingController.expectOne('./assets/config.json');
+
+    req.flush(config)
+
+    httpTestingController.verify();
+
+  });
+});
--- src/Zapisywarka.WEB/libs/shared/domain/src/lib/shared-domain.module.ts
+++ src/Zapisywarka.WEB/libs/shared/domain/src/lib/shared-domain.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormErrorMessagesPipe } from './form-error-messages/form-error-messages.pipe';
+import { ConfigurationService } from './configuration/configuration.service';
+
 
 @NgModule({
   imports: [CommonModule],
   declarations: [FormErrorMessagesPipe],
   exports: [FormErrorMessagesPipe],
 })
-export class SharedDomainModule {}
+export class SharedDomainModule {
+
+   public static forRoot(): ModuleWithProviders<SharedDomainModule> {
+
+    return {
+      ngModule: SharedDomainModule,
+      providers: [{
+        provide: APP_INITIALIZER,
+        useFactory: (confService: ConfigurationService) => () => confService.loadConfig(),
+        deps: [ConfigurationService]
+      }]
+    }
+
+  }
+}
