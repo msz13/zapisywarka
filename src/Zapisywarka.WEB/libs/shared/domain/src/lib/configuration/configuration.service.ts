554f655cab9be75b3de97b14432d2a80228949c4
msz13 <mat.szczecinski@gmail.com>
2021-08-26 21:13:45 +0200

    feat(web-shared-domain) implemented configuration service
    service should load configuration from file 'assets/config.json
    
    task: #31

---
+++ src/Zapisywarka.WEB/libs/shared/domain/src/lib/configuration/configuration.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+
+interface Config {
+  auth: {
+    issuer: string,
+    clientId: string,
+  },
+  signUpURL: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigurationService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  private config: Config
+
+  loadConfig() {
+    return this.http.get('./assets/config.json')
+        .toPromise()
+        .then((config: any) => {
+            this.config = config;            
+        })
+        .catch((err: any) => {
+            console.error(err);
+        })
+
+  }
+
+  getConfig(): Config {
+    return this.config
+  }
+  
+}
