f5f2b4d2bb70d976890226886aef09b65912e9f1
msz13 <mat.szczecinski@gmail.com>
2021-09-18 01:59:42 +0200

    feat(web-zapisywarka-rejestracja) add route to login feature (#43) (#12)
    close #43

--- src/Zapisywarka.WEB/apps/zapisywarka-rejestracja-e2e/src/integration/user-registration/user-registration/user-registration.step.ts
+++ src/Zapisywarka.WEB/apps/zapisywarka-rejestracja-e2e/src/integration/user-registration/user-registration/user-registration.step.ts
@@ -1,97 +1,106 @@
-import { NavigationDriver } from "../../../support/drivers/ui/navigation";
-import { OrganiserSignUpDriver} from "../../../support/drivers/ui/organizer-regstration";
-import { And, Before, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-import { RestOrganiserSignUpDriver } from "apps/zapisywarka-rejestracja-e2e/src/support/drivers/rest/RestOrganiserSignUpDriver";
+import { NavigationDriver } from '../../../support/drivers/ui/navigation';
+import { OrganiserSignUpDriver } from '../../../support/drivers/ui/organizer-regstration';
+import {
+  And,
+  Before,
+  Given,
+  Then,
+  When,
+} from 'cypress-cucumber-preprocessor/steps';
+import { RestOrganiserSignUpDriver } from 'apps/zapisywarka-rejestracja-e2e/src/support/drivers/rest/RestOrganiserSignUpDriver';
 
+let driver: OrganiserSignUpDriver;
+let navigationDriver: NavigationDriver;
+let restDriver: RestOrganiserSignUpDriver;
+let token: string;
+let uniqueId: string;
 
+const uniqueName = (userName: string) => userName + '.' + uniqueId;
 
+Before(() => {
+  driver = new OrganiserSignUpDriver();
+  restDriver = new RestOrganiserSignUpDriver();
+  navigationDriver = new NavigationDriver();
+  uniqueId = Date.now().toString();
+  navigationDriver.navigate('/sign-up');
+  cy.intercept('POST', 'api/identity/users').as('createUser');
+});
 
-let driver: OrganiserSignUpDriver
-let navigationDriver: NavigationDriver
-let restDriver: RestOrganiserSignUpDriver
-let token: string 
-let uniqueId: string 
+Given('Stworzono następujący kod dostępu: {string}', (givenToken) => {
+  //Utworzono przy inicjacji bazy
+  token = givenToken;
+});
 
-const uniqueName = (userName: string)=> userName+'.'+uniqueId
+Given('Organizator zapisów podał nazwę użytkownika {string}', (userName) => {
+  driver.enterUserName(userName);
+});
 
-Before(()=>{
-    driver = new OrganiserSignUpDriver()
-    restDriver = new RestOrganiserSignUpDriver()
-    navigationDriver = new NavigationDriver()
-    uniqueId =  Date.now().toString()
-    navigationDriver.navigate('/sign-up')
-    cy.intercept('POST', 'api/identity/users').as('createUser')
-   
-  
-})
+Given('Organizator zapisów podał kod dostępu {string}', (accesCode) => {
+  driver.enterAccessCode(accesCode);
+});
 
+Given('Baza użytkowników zawiera następujących organizatorów', (organisers) => {
+  const userName = uniqueName(organisers.hashes()[0].Nazwa_uzytkownika);
+  console.log('token: ', token);
+  restDriver.createUser(token, userName, 'Password_01');
+});
 
-Given('Stworzono następujący kod dostępu: {string}', (givenToken)=>{
-   //Utworzono przy inicjacji bazy 
-   token = givenToken
-   
-})
+Given('Organizator zapisów podał hasło {string}', (password) => {
+  driver.enterPassword(password);
+});
 
-Given("Organizator zapisów podał nazwę użytkownika {string}", (userName)=>{
-    driver.enterUserName(userName)
- })
+And('Organizator zapisów podał kod dostępu {string}', (accessToken) => {
+  driver.enterAccessCode(accessToken);
+});
 
+And(
+  'Organizator zapisów wypełnił dane rejestracji konta',
+  (registrationData) => {
+    const {
+      Nazwa_użytkownika: userName,
+      Hasło: password,
+      Potwierdzenie_hasła: passwordConfirmation,
+    } = registrationData.hashes()[0];
 
-Given('Organizator zapisów podał kod dostępu {string}', (accesCode)=>{
-    driver.enterAccessCode(accesCode)
-})
+    driver.enterRegistrationData(
+      uniqueName(userName),
+      password,
+      passwordConfirmation
+    );
+  }
+);
 
-Given("Baza użytkowników zawiera następujących organizatorów", (organisers)=> {
-    const userName = uniqueName(organisers.hashes()[0].Nazwa_uzytkownika)
-    console.log('token: ', token)
-    restDriver.createUser(token, userName, 'Password_01')
-})
+And('Organizator zapisów podaje nazwię użytkownika {string}', (userName) => {
+  const uniqueUserName = uniqueName(userName);
 
-Given("Organizator zapisów podał hasło {string}", password => {
-    driver.enterPassword(password)
-})
+  driver.enterUserName(uniqueUserName);
+});
 
-And("Organizator zapisów podał kod dostępu {string}", (accessToken)=>{
-    driver.enterAccessCode(accessToken)
-})
+When('Próbuję się zarejestrować', () => {
+  driver.signUp();
+});
 
-And('Organizator zapisów wypełnił dane rejestracji konta', (registrationData)=>{
-    
-    const {Nazwa_użytkownika: userName, Hasło: password, Potwierdzenie_hasła: passwordConfirmation } = registrationData.hashes()[0]
-    
-    driver.enterRegistrationData(uniqueName(userName), password, passwordConfirmation)
-})
+When('Podaje potwierdzenie hasła {string}', (passwordConfirmation) => {
+  driver.enterPasswordConfirmation(passwordConfirmation);
+});
 
-And("Organizator zapisów podaje nazwię użytkownika {string}", (userName) =>{
-    const uniqueUserName = uniqueName(userName)
+Then(
+  'Baza użytkowników zawiera organizatora zapisów o imieniu {string}',
+  (userName) => {
+    restDriver
+      .getOrganisers()
+      .should('deep.include', { username: uniqueName(userName) });
+  }
+);
 
-    driver.enterUserName(uniqueUserName)
-})
-
-When('Próbuję się zarejestrować', ()=>{
-   driver.signUp()
-})
-
-
-When("Podaje potwierdzenie hasła {string}", passwordConfirmation => {
-    driver.enterPasswordConfirmation(passwordConfirmation)
-})
-
-Then('Baza użytkowników zawiera organizatora zapisów o imieniu {string}', (userName)=>{
-    
-    restDriver.getOrganisers().should('deep.include', {username: uniqueName(userName)}) 
-    
-})
-
-
-Then('Nie może procedować rejestracji i widzi komunikat {string}', (userName)=>{
-    
-    navigationDriver.ShouldSeeSignUpPage()
+Then(
+  'Nie może procedować rejestracji i widzi komunikat {string}',
+  (userName) => {
+    navigationDriver.ShouldSeeSignUpPage();
     //TODO dodać sprawdzenie komunikatu
-    
-})
+  }
+);
 
-
-And('Przekierowany jest na stronę logowania', ()=>{
-    navigationDriver.ShouldVisitLoginPage()
-})
+And('Przekierowany jest na stronę logowania', () => {
+  navigationDriver.ShouldVisitLoginPage();
+});
--- src/Zapisywarka.WEB/libs/landing-page/src/lib/landing-page.module.ts
+++ src/Zapisywarka.WEB/libs/landing-page/src/lib/landing-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import {SharedMaterialModule} from '@zapisywarka.web/material'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, SharedMaterialModule],
+  imports: [CommonModule, SharedMaterialModule, RouterModule],
   declarations: [LandingPageComponent],
   exports: [LandingPageComponent]
 })
