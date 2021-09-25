100eebe55859f49b5768a4b8fdd15ce9c00536c9
msz13 <mat.szczecinski@gmail.com>
2021-09-25 23:37:47 +0200

    feat(web-login-feature) update login fom view (49)

--- src/Zapisywarka.WEB/libs/identity/login-feature/src/lib/login-container.component.ts
+++ src/Zapisywarka.WEB/libs/identity/login-feature/src/lib/login-container.component.ts
@@ -1,12 +1,24 @@
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
-  template: ` <p>login-container works!</p> `,
+  selector: 'app-login-containter',
+  template: `<p>login-container works!{{test}}</p>
+  <button (click)="onClick()">test</button>
+  `,
   styles: [],
 })
 export class LoginContainerComponent implements OnInit {
   constructor() {}
 
+  @Input() test!: string 
+  @Output() clicked = new EventEmitter<string>()
+
   ngOnInit(): void {}
+
+  onClick() {
+    this.clicked.emit(this.test)
+  }
 }
--- src/Zapisywarka.WEB/libs/identity/login-feature/src/lib/login/login.component.stories.ts
+++ src/Zapisywarka.WEB/libs/identity/login-feature/src/lib/login/login.component.stories.ts
@@ -1,97 +1,114 @@
 
-import { LoginComponent } from './login.component';
+import { LoginComponent, LoginData } from './login.component';
 import {SharedMaterialModule} from '@zapisywarka.web/material'
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SharedDomainModule } from '@zapisywarka-client-aps/shared/domain';
+import {Story} from '@storybook/angular'
+import {action} from '@storybook/addon-actions'
+import { IFormGroup, RxwebValidators, RxFormBuilder} from '@rxweb/reactive-form-validators'
+import { Component, Output, EventEmitter } from '@angular/core';
+
+
 
 export default {
   title: 'LoginComponent',
-  argTypes: { submited: {action: 'submit'}}
+  argTypes: { onSubmit: {action: 'submit'}}
 }
 
-const loginFormGroup = new FormGroup({
-  'userName': new FormControl(),
-  'password': new FormControl(),
-  'rememberMe': new FormControl()
-})
-
-export const Template<LoginComponent> = (args) => ({
-  moduleMetadata: {
-    imports: [SharedMaterialModule, ReactiveFormsModule, BrowserAnimationsModule, SharedDomainModule],
-    declarations: [LoginComponent]
-  },
-  component: LoginComponent,
-  template: `<app-login-from [loginForm]="loginForm"></app-login-from>`,
-  props: {
-    loginForm: loginFormGroup
-  }
-})
-
-export const primary = () => ({
-  moduleMetadata: {
-    imports: [SharedMaterialModule, ReactiveFormsModule, BrowserAnimationsModule, SharedDomainModule],
-    declarations: [LoginComponent]
-  },
-  component: LoginComponent,
-  template: `<app-login-from [loginForm]="loginForm"></app-login-from>`,
-  props: {
-    loginForm: loginFormGroup
-  }
-})
 
 
-const filledLoginFormGroup = new FormGroup({
-  'userName': new FormControl("mateusz_szczecinski"),
-  'password': new FormControl("12345678"),
-  'rememberMe': new FormControl(true)
-})
-
-
-export const filled = () => ({
-  moduleMetadata: {
-    imports: [SharedMaterialModule, ReactiveFormsModule, BrowserAnimationsModule, SharedDomainModule],
-    declarations: [LoginComponent]
-  },
-  component: LoginComponent,
-  template: `<app-login-from [loginForm]="loginForm"></app-login-from>`,
-  props: {
-    loginForm: filledLoginFormGroup,
-
-  }
-})
-
-
-const errorLoginFormGroup = new FormGroup({
-  'userName': new FormControl(''),
-  'password': new FormControl(''),
-  'rememberMe': new FormControl(false)
-})
-
-const getErrorForm = ()=>{
-  errorLoginFormGroup.get('userName')?.setErrors({required: {
-    message: "Pole wymagane"
-  }})
+const buildRxForm = ()=> {
   
-  errorLoginFormGroup.get('password')?.setErrors({required: {
-    message: "Pole wymagane"
-  }})
-
-  errorLoginFormGroup.markAllAsTouched()
-
-  return errorLoginFormGroup
+  const builder = new RxFormBuilder()
+  return builder.group({
+    'userName': new FormControl('',[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'password': new FormControl('',[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'rememberMe': new FormControl(false)
+  })
 }
 
+const Template: Story = (args) => {
 
+  const loginFormGroup = buildRxForm() 
 
-export const withError = () => ({
+  loginFormGroup.valueChanges.subscribe((value: any) => action('form')(JSON.stringify(value)))
+  
+  return {
   moduleMetadata: {
     imports: [SharedMaterialModule, ReactiveFormsModule, BrowserAnimationsModule, SharedDomainModule],
     declarations: [LoginComponent]
   },
   component: LoginComponent,
-  template: `<app-login-from [loginForm]="loginForm"></app-login-from>`,
+  template: `<app-login-form [loginForm]="loginForm" (submited)="onSubmit($event)"></app-login-form>`,
   props: {
-    loginForm: getErrorForm()
+    ...args,
+    loginForm: loginFormGroup     
   }
+}
+}
+
+export const Default = Template.bind({})
+
+const buildRxFormWithError = ()=> {
+  
+  const builder = new RxFormBuilder()
+
+  const group = builder.group({
+    'userName': new FormControl(null,[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'password': new FormControl(null,[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'rememberMe': new FormControl(false)
+  })
+
+  group.setErrors({invalidCredentials: {message: 'Niepoprawny login lub hasło'}})
+
+  console.log(JSON.stringify(group.errors))
+
+  return group
+}
+
+/*
+@Component({
+  template: `<app-login-from [loginForm]="loginForm" (submited)="onComponentSubmit($event)"></app-login-from>`,
+  selector: 'test-comp' 
 })
+export class TestComponent {
+
+loginForm: IFormGroup<LoginData>
+onSubmit = new EventEmitter<null>()
+
+constructor() {
+  const builder = new RxFormBuilder()
+  this.loginForm = <IFormGroup<LoginData>>builder.group({
+    'userName': new FormControl('',[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'password': new FormControl('',[RxwebValidators.required({message: 'Pole jest wymagane'})]),
+    'rememberMe': new FormControl(false)
+  })
+
+}
+
+onComponentSubmit() {
+this.loginForm.setErrors({invalidCredentials: {message: 'Nieporawny login lub hasło'}})
+this.onSubmit.emit(null)
+}
+
+
+}
+
+export const ServerError: Story<LoginComponent> = (args) => {
+
+    
+  return {
+  moduleMetadata: {
+    imports: [SharedMaterialModule, ReactiveFormsModule, BrowserAnimationsModule, SharedDomainModule],
+    declarations: [LoginComponent, TestComponent]
+  },
+  component: LoginComponent,
+  template: `<test-comp></test-comp>`,
+  props: {
+      
+  }
+}
+}
+
+*/
