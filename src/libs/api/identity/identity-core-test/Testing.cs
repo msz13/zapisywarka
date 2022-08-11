1eaad3b687d3b51048d855bb10d362b8b4e379e3
msz13 <41579953+msz13@users.noreply.github.com>
2022-08-11 23:31:57 +0200

    Revert "ci: add nx to commit workflow for web and api jobs (#106) (story-#101)"
    This reverts commit 3ce39e890b8f8e45adcdcdb4a4995f25839fc88d.

--- src/libs/api/identity/identity-core-test/Testing.cs
+++ src/libs/api/identity/identity-core-test/Testing.cs
@@ -14,9 +14,6 @@ using Zapisywarka.API.Modules.Identity.Core.Infrastructure;
 using Zapisywarka.API.Common.Infrastructure.Persistance;
 using Microsoft.AspNetCore.Http;
 using Moq;
-using Ductus.FluentDocker.Builders;
-using Ductus.FluentDocker.Services;
-
 namespace Zapisywarka.Api.Modules.Identity.IntegrationTests
 {
   [SetUpFixture]
@@ -25,8 +22,6 @@ namespace Zapisywarka.Api.Modules.Identity.IntegrationTests
     private static IConfigurationRoot _configuration;
     private static IServiceScopeFactory _scopeFactory;
     private static Checkpoint _checkpoint;
-
-    private static ICompositeService _dbContainer;
 
 
     [OneTimeSetUp]
@@ -62,7 +57,17 @@ namespace Zapisywarka.Api.Modules.Identity.IntegrationTests
         DbAdapter = DbAdapter.Postgres
       };
     }
-   
+
+    private static void BuildConfiguration()
+    {
+      var builder = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("/home/msz13/programowanie/zapisywarka/src/Zapisywarka.API/Modules/Identity/Zapisywarka.Api.Modules.Identity.IntegrationTests/appsettings.json", true, true)
+                      .AddEnvironmentVariables();
+
+      _configuration = builder.Build();
+    }
+
     private void ConfigureCommonServices(IServiceCollection services)
     {
       services.AddScoped(
@@ -77,17 +82,6 @@ namespace Zapisywarka.Api.Modules.Identity.IntegrationTests
 
       var httpContext = new Mock<HttpContext>();
 
-    }
-
-    private void StartDatabase() 
-    {
-      var path = Path.Combine(Directory.GetCurrentDirectory(), "docker-compose-postgresql.yml");
-
-      _dbContainer = new Builder().UseContainer()
-        .UseCompose()
-        .FromFile(path)
-        .Build()
-        .Start();
 
     }
 
