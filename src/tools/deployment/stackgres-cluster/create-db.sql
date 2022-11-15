39f7981c4595c505362b74a53a9c3ff8da2eee14
msz13 <mat.szczecinski@gmail.com>
2022-11-15 22:27:23 +0100

    add stackgres postgresql operator

---
+++ src/tools/deployment/stackgres-cluster/create-db.sql
@@ -0,0 +1,4 @@
+create user dbowner password 'ZDJmMmE2Mzc3ZjJjYTI4ZTk0MDc4OWU4';
+create user dbuser password 'YTNlMzNmMjVmMTM0MWVmODVlNjhmYmU3' ;
+alter user dbowner with superuser;
+create database zapisywarka owner dbowner;
